d51bcc77ac454d65c901ce853fb28fc15711aa42
malathimahendiran <malathimahendiran2004@gmail.com>
2026-06-23 20:14:46 +0530

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class Sample1{
+    public static void main(string[]args){
+        int english,tamil,maths,science,social,totalmarks;
+        string student name;
+        scanner sc=new scanner(system.in);
+        System.out.println("enter the student name");
+        student name=sc.nextline();
+        System.out.println("enter the value of english mark ");
+        english mark=sc.nextline();
+        System.out.println("enter the value of tamil mark ");
+        tamil mark=sc.nextline();
+        System.out.println("enter the value of maths mark ");
+        maths mark=sc.nextline();
+        System.out.println("enter the value of science mark ");
+        science mark=sc.nextline();
+        System.out.println("enter the value of social mark ");
+        social mark=sc.nextline();
+        System.out.println("enter the total marks")
+        total marks=sc.nextline();
+        System.out.println("student name:"+student name);
+        System.out.println("value of english mark:"+english);
+        System.out.println("value of tamil mark:"+tamil);
+        System.out.println("value of maths mark:"+maths);
+        System.out.println("value of science mark:"+science);
+        System.out.println("value of social mark:"+social);
+        System.out.println("value of total mark:"+total);
+
+        
+        
+        
+
+        
+
+        
+
+
+    }
+}
